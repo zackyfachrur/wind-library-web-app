713561392ee154c958fcc7a1863be9a15b9431ea
mzackyfa <116891033+zackyfachrur@users.noreply.github.com>
2024-08-11 18:07:30 +0700

    Delete src/db directory

--- src/db/Book.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('Connection.php');
-
-$tampilkan_buku = mysqli_query($connection, "SELECT * FROM tabel_buku");
-$results = mysqli_fetch_all($tampilkan_buku, MYSQLI_ASSOC);
-
-if (!$tampilkan_buku) {
-   echo "Gagal menampilkan buku";
-}
